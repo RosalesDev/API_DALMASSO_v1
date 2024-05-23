611e44fc8d3afe280fd0383f8e76eb8be691357a
Luis <alu.magallanes@gmail.com>
2024-05-23 10:52:25 -0300

    cambios de URL

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const server = express();
 
 server.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
